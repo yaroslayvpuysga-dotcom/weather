e8bbf37849b51d123d08b8d8222d0fe83e44a11b
yaroslayvpuysga-dotcom <yaroslayvpuysga@gmail.com>
2026-04-17 13:38:34 +0300

    add vercel.json CSP

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// HilltopAds
+const s = document.createElement('script');
+s.src = "//stupid-police.com/bgXtV.scdRGVlK0FY/W/ct/he/mM9QuZZaUllHk/P/T_Yk5WNxjOYEyeMQznMhtgNuj/kS2/NdjaIBzjNIwx";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+document.head.appendChild(s);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
